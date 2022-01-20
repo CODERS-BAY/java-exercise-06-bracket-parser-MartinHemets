349a9169c44f1cc5c8ecc7907213d2c07cbea073
Martin <martin_hemetsberger@gmx.at>
2022-01-20 21:06:49 +0100

    Done

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -3,5 +3,12 @@ package application;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Parser");
+
+    Parser parser = new Parser();
+        try {
+            parser.parseFile("sample1.txt");
+        } catch (ParsingException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/application/Parser.java
+++ src/main/java/application/Parser.java
@@ -32,9 +32,27 @@ public class Parser {
     }
 
     private void parse(Path path) throws ParsingException {
+        int klauf=0;
+        int klzu=0;
         try (Stream<String> lineStream = Files.lines(path)) {
+
             for (String line : lineStream.toList()) {
-                // get the line as string
+                for (int i = 0; i < line.length(); i++) {
+                    elementStack.add(line.charAt(i));
+                    if(line.charAt(i)=='{'){
+                        klauf++;
+                    }
+                    if(line.charAt(i)=='}'){
+                        klzu++;
+                    }
+                    if(klzu>klauf){
+                        throw new ParsingException("Error parsing the file. { expected");
+                    }
+                }
+                System.out.println("Kl auf: "+klauf+"  Kl zu: "+klzu);
+            }
+            if(klauf>klzu){
+                throw new ParsingException("Error parsing the file. } expected");
             }
         } catch (IOException e) {
             System.err.println(e.getMessage());
